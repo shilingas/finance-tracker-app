2cb2b15e1b9cf13820554143ec904b569957b7db
PijusOz <pijus.ozelis@gmail.com>
2022-09-12 13:50:04 +0300

    :(

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace FinanceTracker
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            Console.WriteLine("B");
         }
     }
 }
